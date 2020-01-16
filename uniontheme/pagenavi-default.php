0776d2dfbfe16238d8421cf04d96241621fea0ff
takuro yamao <yamao@unionnet.jp>
2020-01-16 17:49:22 +0900

    ambrosite-nextprevious-post-link-plusが無くなってた為 カスタム投稿用ページャーのコード修正

--- uniontheme/pagenavi-default.php
+++ uniontheme/pagenavi-default.php
@@ -2,9 +2,9 @@
 
 <?php if('post'!=get_post_type()) { $post_type = get_post_type( $post->ID ); //標準投稿以外のときに適用 ?>
 <div class="wp-pagenavi">
-<?php if(function_exists('previous_post_link_plus')) { previous_post_link_plus( array('post_type' =>  '"'.$post_type.'"', 'format' => '<div class="alignleft">%link</div>', 'link' => '&laquo; 前へ') ); } ?>
+<?php previous_post_link('<div class="alignleft">%link</div>', '&laquo; 前へ', ''); ?>
 <div class="center"><a href="<?php echo esc_url(get_post_type_archive_link($post_type)); ?>">一覧へ</a></div>
-<?php if(function_exists('next_post_link_plus')) { next_post_link_plus( array('post_type' =>  '"'.$post_type.'"', 'format' => '<div class="alignright">%link</div>', 'link' => '次へ &raquo;') ); } ?>
+<?php next_post_link('<div class="alignright">%link</div>', '次へ &raquo;', ''); ?>
 <!--/ .wp-pagenavi --></div>
 
 <?php } else{ //標準投稿に適用 ?>
